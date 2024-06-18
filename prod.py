5aba50cbf857f03200e5f31e0f7a75313fe7eb62
Jaho_HE <jaho_he@outlook.com>
2024-06-18 17:57:29 +0800

    已经可以连上mongo了，但写入不了（还得研究）；正优化待优化的数据；

--- prod.py
+++ prod.py
@@ -1,40 +1,56 @@
 import requests
 from pymongo import MongoClient
+from bs4 import BeautifulSoup
 
 
 class taidiDetail:
     def __init__(self):
-        self.jobList_url = "https://www.5iai.com/api/enterprise/job/public/es"
-        self.jobDetail_url = (
+        self.jobListApi_url = "https://www.5iai.com/api/enterprise/job/public/es"
+        self.jobDetailApi_url = (
             "https://www.5iai.com/api/enterprise/job/public"  # 详情页数据接口
         )
-        myClient = MongoClient("mongodb://23.158.104.186/32:27017")
+
+        uri = "mongodb+srv://Chil:0705@childb.zq9crau.mongodb.net/?retryWrites=true&w=majority&appName=ChilDB"
+        self.client = MongoClient(uri)
 
     # end def
     def getDetailID(self):
         detailIdList = []
+        detailList = []
+        obj = {"detailIdList": [], "detailList": []}
         for j in range(0, 18):
             data = {"pageSize": 10, "pageNum": j}
-            r = requests.get(self.jobList_url, params=data).json()
+            r = requests.get(self.jobListApi_url, params=data).json()
             content = r.get("data").get("content")
+
             for i in range(len(content)):
                 detailId = content[i].get("id")  # 页面详情id
                 detailIdList.append(detailId)
-        return detailIdList
+                detailList.append(content[i])
+        obj["detailList"] = detailList
+        obj["detailIdList"] = detailIdList
+        return obj
 
     # end def
-    def getDetailInfo(self, detailIdList):
+    def getDetailInfo(self, obj):
+        content = obj["detailList"]
+        detailIdList = obj["detailIdList"]
         detailInfo = []
         for i in range(len(detailIdList)):
             detailId = detailIdList[i]  # 页面详情id
             detailData = {"id": detailId}
-            detailR = requests.get(self.jobDetail_url, params=detailData).json()
+            detailR = requests.get(self.jobDetailApi_url, params=detailData).json()
             detailData = detailR.get("data")
 
             obj = {}
+            obj["_id"] = detailIdList[i]  # 详情id
             obj["公司名称"] = detailData.get("enterpriseName")  # 公司名称
             obj["招聘人数"] = detailData.get("count")  # 招聘人数
-            obj["职位描述"] = detailData.get("jobRequiredments")  # 职位描述（待优化）
+            obj["职位描述"] = detailData.get("jobRequiredments")  # 职位描述（待优化）.
+            jRhtml = BeautifulSoup(obj["职位描述"], "html.parser")
+
+            print(jRhtml.find_all(name="p"))  # 以标签名为p查询元素
+
             obj["职位福利"] = detailData.get("welfare")  # 职位福利(待优化)
             obj["发布时间"] = detailData.get("publishTime")  # 发布时间
             obj["截止时间"] = detailData.get("deadline")  # 截止时间
@@ -47,49 +63,60 @@ class taidiDetail:
                         obj["技能要求"] + "、" + str(skillsList[j].get("labelName"))
                     )
 
-            educationalRequirements = self.content[i].get(
+            educationalRequirements = content[i].get(
                 "educationalRequirements"
             )  # 学历要求码
             # 2 大专 1 技工 3 本科
-            match (educationalRequirements):  # 学历
+            match (educationalRequirements):  # 学历要求
+                case 0:
+                    obj["学历要求"] = "不限"
                 case 1:
                     obj["学历要求"] = "技工"
                 case 2:
                     obj["学历要求"] = "大专"
                 case 3:
                     obj["学历要求"] = "本科"
+                case 4:
+                    obj["学历要求"] = "硕士"
+                case 5:
+                    obj["学历要求"] = "博士"
             # end match
-            obj["最高薪资"] = self.content[i].get("maximumWage")  # 最高薪资
-            obj["最低薪资"] = self.content[i].get("minimumWage")  # 最低薪资
-            obj["经验要求"] = self.content[i].get("exp")  # 经验要求
-            obj["岗位名称"] = self.content[i].get("positionName")  # 岗位名称
+            obj["最高薪资"] = content[i].get("maximumWage")  # 最高薪资
+            obj["最低薪资"] = content[i].get("minimumWage")  # 最低薪资
+            obj["经验要求"] = content[i].get("exp")  # 经验要求
+            obj["岗位名称"] = content[i].get("positionName")  # 岗位名称
             obj["单位性质"] = (
-                self.content[i].get("enterpriseExtInfo").get("econKind")
+                content[i].get("enterpriseExtInfo").get("econKind")
             )  # 单位性质
             obj["所属行业"] = (
-                self.content[i].get("enterpriseExtInfo").get("industry")
+                content[i].get("enterpriseExtInfo").get("industry")
             )  # 所属行业（待优化）
             obj["公司人数"] = (
-                self.content[i].get("enterpriseExtInfo").get("personScope")
+                content[i].get("enterpriseExtInfo").get("personScope")
             )  # 公司人数
             # print(obj)
+
             detailInfo.append(obj)
         return detailInfo
 
     # end def
     def save(self, detailInfo):
-        db = self.client.ChilDB
-        coll = db.taidi
-        coll.insert_test_Info(detailInfo)
-        print("保存成功")
-        result = coll.find()
-        for doc in result:
-            print(doc)
+        database = self.client.taidiInfo  # 数据库名称
+        coll = database.detailInfo
+        # print(coll)
+        # coll.insert_many(detailInfo)
+        # print(f"==>> detailInfo: {detailInfo}")
+        # print("保存成功")
+        # results = database.find({})
+        # for result in results:
+        #     print(result)
+
         # end def
 
     def run(self):
-        detailIdList = self.getDetailID()
-        detailInfo = self.getDetailInfo(detailIdList)
+        obj = self.getDetailID()
+        detailInfo = self.getDetailInfo(obj)
+        # print(detailInfo)
         self.save(detailInfo)
         # end def
 
--- test1.py
+++ test1.py
@@ -3,40 +3,40 @@ from bs4 import BeautifulSoup  # 网页解析，获取数据
 import re  # 正则表达式，进行文字匹配`
 import urllib.request, urllib.error  # 制定URL，获取网页数据
 import xlwt  # 进行excel操作
-#import sqlite3  # 进行SQLite数据库操作
 
-findLink = re.compile(r'<a href="(.*?)">')  # 创建正则表达式对象，标售规则   影片详情链接的规则
+# import sqlite3  # 进行SQLite数据库操作
+
+findLink = re.compile(
+    r'<a href="(.*?)">'
+)  # 创建正则表达式对象，标售规则   影片详情链接的规则
 findImgSrc = re.compile(r'<img.*src="(.*?)"', re.S)
 findTitle = re.compile(r'<span class="title">(.*)</span>')
 findRating = re.compile(r'<span class="rating_num" property="v:average">(.*)</span>')
-findJudge = re.compile(r'<span>(\d*)人评价</span>')
+findJudge = re.compile(r"<span>(\d*)人评价</span>")
 findInq = re.compile(r'<span class="inq">(.*)</span>')
 findBd = re.compile(r'<p class="">(.*?)</p>', re.S)
 
 
-
-
 def main():
-    baseurl = "https://movie.douban.com/top250?start="  #要爬取的网页链接
+    baseurl = "https://movie.douban.com/top250?start="  # 要爬取的网页链接
     # 1.爬取网页
     datalist = getData(baseurl)
-    savepath = "豆瓣电影Top250.xls"    #当前目录新建XLS，存储进去
+    savepath = "豆瓣电影Top250.xls"  # 当前目录新建XLS，存储进去
     # dbpath = "movie.db"              #当前目录新建数据库，存储进去
     # 3.保存数据
-    saveData(datalist,savepath)      #2种存储方式可以只选择一种
+    saveData(datalist, savepath)  # 2种存储方式可以只选择一种
     # saveData2DB(datalist,dbpath)
-
 
 
 # 爬取网页
 def getData(baseurl):
-    datalist = []  #用来存储爬取的网页信息
+    datalist = []  # 用来存储爬取的网页信息
     for i in range(0, 10):  # 调用获取页面信息的函数，10次
         url = baseurl + str(i * 25)
         html = askURL(url)  # 保存获取到的网页源码
         # 2.逐一解析数据
         soup = BeautifulSoup(html, "html.parser")
-        for item in soup.find_all('div', class_="item"):  # 查找符合要求的字符串
+        for item in soup.find_all("div", class_="item"):  # 查找符合要求的字符串
             data = []  # 保存一部电影所有信息
             item = str(item)
             link = re.findall(findLink, item)[0]  # 通过正则表达式查找
@@ -44,14 +44,14 @@ def getData(baseurl):
             imgSrc = re.findall(findImgSrc, item)[0]
             data.append(imgSrc)
             titles = re.findall(findTitle, item)
-            if (len(titles) == 2):
+            if len(titles) == 2:
                 ctitle = titles[0]
                 data.append(ctitle)
-                otitle = titles[1].replace("/", "")  #消除转义字符
+                otitle = titles[1].replace("/", "")  # 消除转义字符
                 data.append(otitle)
             else:
                 data.append(titles[0])
-                data.append(' ')
+                data.append(" ")
             rating = re.findall(findRating, item)[0]
             data.append(rating)
             judgeNum = re.findall(findJudge, item)[0]
@@ -63,8 +63,8 @@ def getData(baseurl):
             else:
                 data.append(" ")
             bd = re.findall(findBd, item)[0]
-            bd = re.sub('<br(\s+)?/>(\s+)?', "", bd)
-            bd = re.sub('/', "", bd)
+            bd = re.sub("<br(\s+)?/>(\s+)?", "", bd)
+            bd = re.sub("/", "", bd)
             data.append(bd.strip())
             datalist.append(data)
 
@@ -92,19 +92,29 @@ def askURL(url):
 
 
 # 保存数据到表格
-def saveData(datalist,savepath):
+def saveData(datalist, savepath):
     print("save.......")
-    book = xlwt.Workbook(encoding="utf-8",style_compression=0) #创建workbook对象
-    sheet = book.add_sheet('豆瓣电影Top250', cell_overwrite_ok=True) #创建工作表
-    col = ("电影详情链接","图片链接","影片中文名","影片外国名","评分","评价数","概况","相关信息")
-    for i in range(0,8):
-        sheet.write(0,i,col[i])  #列名
-    for i in range(0,250):
+    book = xlwt.Workbook(encoding="utf-8", style_compression=0)  # 创建workbook对象
+    sheet = book.add_sheet("豆瓣电影Top250", cell_overwrite_ok=True)  # 创建工作表
+    col = (
+        "电影详情链接",
+        "图片链接",
+        "影片中文名",
+        "影片外国名",
+        "评分",
+        "评价数",
+        "概况",
+        "相关信息",
+    )
+    for i in range(0, 8):
+        sheet.write(0, i, col[i])  # 列名
+    for i in range(0, 250):
         # print("第%d条" %(i+1))       #输出语句，用来测试
         data = datalist[i]
-        for j in range(0,8):
-            sheet.write(i+1,j,data[j])  #数据
-    book.save(savepath) #保存
+        for j in range(0, 8):
+            sheet.write(i + 1, j, data[j])  # 数据
+    book.save(savepath)  # 保存
+
 
 # def saveData2DB(datalist,dbpath):
 #     init_db(dbpath)
@@ -151,14 +161,8 @@ def saveData(datalist,savepath):
 # 保存数据到数据库
 
 
-
-
-
-
-
 if __name__ == "__main__":  # 当程序执行时
     # 调用函数
-     main()
+    main()
     # init_db("movietest.db")
-     print("爬取完毕！")
-
+    print("爬取完毕！")
