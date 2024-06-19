89c7dd163ab7178aca29ae762e5298c37886d694
Jaho_HE <jaho_he@outlook.com>
2024-06-19 11:46:34 +0800

    全部完成

--- prod.py
+++ prod.py
@@ -44,34 +44,8 @@ class taidiDetail:
             detailData = detailR.get("data")
 
             obj = {}
-            # obj["_id"] = detailIdList[i]  # 详情id
-            obj["公司名称"] = detailData.get("enterpriseName")  # 公司名称
-            obj["招聘人数"] = detailData.get("count")  # 招聘人数
 
-            jRhtml = BeautifulSoup(detailData.get("jobRequiredments"), "html.parser")
-            jRhtmlList = jRhtml.find_all(name="p")  # 获取每一个p标签
-            jobRequiredments = ""
-            # 循环用get_text（）获取标签中的文字
-            for n in range(len(jRhtmlList)):
-                if n == 0:
-                    jobRequiredments = jRhtmlList[n].get_text()
-                else:
-                    jobRequiredments = jobRequiredments + jRhtmlList[n].get_text()
-            obj["职位描述"] = jobRequiredments  # 职位描述
-
-            welfareStrlist = detailData.get("welfare").split(",")
-            obj["职位福利"] = self.dataProcessing(welfareStrlist, ",")  # 职位福利
-
-            obj["发布时间"] = detailData.get("publishTime")  # 发布时间
-            obj["截止时间"] = detailData.get("deadline")  # 截止时间
-            skillsList = detailData.get("skillsList")  # 技能要求
-            for j in range(len(skillsList)):
-                if j == 0:
-                    obj["技能要求"] = str(skillsList[j].get("labelName"))
-                else:
-                    obj["技能要求"] = (
-                        obj["技能要求"] + "、" + str(skillsList[j].get("labelName"))
-                    )
+            obj["岗位名称"] = content[i].get("positionName")  # 岗位名称
 
             educationalRequirements = content[i].get(
                 "educationalRequirements"
@@ -88,22 +62,63 @@ class taidiDetail:
                 obj["学历要求"] = "硕士"
             elif educationalRequirements == 5:
                 obj["学历要求"] = "博士"
-            obj["最高薪资"] = content[i].get("maximumWage")  # 最高薪资
-            obj["最低薪资"] = content[i].get("minimumWage")  # 最低薪资
+
             obj["经验要求"] = content[i].get("exp")  # 经验要求
-            obj["岗位名称"] = content[i].get("positionName")  # 岗位名称
-            obj["单位性质"] = (
-                content[i].get("enterpriseExtInfo").get("econKind")
-            )  # 单位性质
+
+            skillsList = detailData.get("skillsList")  # 技能要求
+            for j in range(len(skillsList)):
+                if j == 0:
+                    obj["技能要求"] = str(skillsList[j].get("labelName"))
+                else:
+                    obj["技能要求"] = (
+                        obj["技能要求"] + "、" + str(skillsList[j].get("labelName"))
+                    )
+
+            obj["招聘人数"] = detailData.get("count")  # 招聘人数
+
+            obj["薪资范围"] = (
+                str(content[i].get("minimumWage"))
+                + " ~ "
+                + str(content[i].get("maximumWage"))
+            )  # 薪资范围 = 最低薪资 + 最高薪资
+
+            welfareStrlist = detailData.get("welfare").split(",")
+            obj["职位福利"] = self.dataProcessing(welfareStrlist, ",")  # 职位福利
+
+            jRhtml = BeautifulSoup(detailData.get("jobRequiredments"), "html.parser")
+            jRhtmlList = jRhtml.find_all(name="p")  # 获取每一个p标签
+            jobRequiredments = ""
+            # 循环用get_text（）获取标签中的文字
+            for n in range(len(jRhtmlList)):
+                if n == 0:
+                    jobRequiredments = jRhtmlList[n].get_text()
+                else:
+                    jobRequiredments = jobRequiredments + jRhtmlList[n].get_text()
+            obj["职位描述"] = jobRequiredments  # 职位描述
+
+            obj["公司名称"] = detailData.get("enterpriseName")  # 公司名称
+
+            obj["公司人数"] = (
+                content[i].get("enterpriseExtInfo").get("personScope")
+            )  # 公司人数
 
             industryList = (
                 content[i].get("enterpriseExtInfo").get("industry").split(",")
             )
             obj["所属行业"] = self.dataProcessing(industryList, "/")  # 所属行业
 
-            obj["公司人数"] = (
-                content[i].get("enterpriseExtInfo").get("personScope")
-            )  # 公司人数
+            obj["单位性质"] = (
+                content[i].get("enterpriseExtInfo").get("econKind")
+            )  # 单位性质
+
+            obj["单位地址"] = detailData.get("enterpriseAddress").get(
+                "detailedAddress"
+            )  # 发布时间
+
+            obj["发布时间"] = detailData.get("publishTime")  # 发布时间
+
+            obj["截止时间"] = detailData.get("deadline")  # 截止时间
+
             # print(obj)
 
             detailInfo.append(obj)
@@ -128,7 +143,7 @@ class taidiDetail:
     # end def
     def save(self, detailInfo):
         database = self.client.taidiInfo  # 数据库名称
-        coll = database.detailInfo
+        coll = database.detailInfo1
         coll.insert_many(detailInfo)
         print("保存成功")
         results = coll.find()
